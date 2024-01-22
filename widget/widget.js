4549664aa5a606511801b1d3413cc3692504d738
Lukasz Walaszczyk <lukasz.walaszczyk@creativestyle.pl>
2024-01-22 08:20:33 +0100

    Update widget

--- widget/widget.js
+++ widget/widget.js
@@ -4,15 +4,17 @@
   app.id = "app";
   document.body.appendChild(app);
 
+  const random = Math.random().toString(36).substring(7);
+
   const module = document.createElement("script");
   module.type = "module";
   module.crossOrigin = "anonymous";
-  module.src = "https://bukasz.github.io/utils/widget/assets/index-xJF6-1RZ.js";
+  module.src = "https://bukasz.github.io/utils/widget/assets/index-abwghORs.js" + "?v=" + random;
   document.head.appendChild(module);
 
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.crossOrigin = "anonymous";
-  link.href = "https://bukasz.github.io/utils/widget/assets/index-vynvvFJq.css";
+  link.href = "https://bukasz.github.io/utils/widget/assets/index-P6GhZm0W.css" + "?v=" + random;
   document.head.appendChild(link);
 })();
