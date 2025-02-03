0191a0000eaf13800efe96940eea99b190cdd0f7
Lukasz Walaszczyk <lukasz.walaszczyk@creativestyle.pl>
2025-02-03 08:22:25 +0100

    Update widget

--- widget/widget.js
+++ widget/widget.js
@@ -9,12 +9,12 @@
   const module = document.createElement("script");
   module.type = "module";
   module.crossOrigin = "anonymous";
-  module.src = "https://bukasz.github.io/utils/widget/assets/index-FpWLSAgY.js?v=" + random;
+  module.src = "https://bukasz.github.io/utils/widget/assets/index-B7N6B8al.js?v=" + random;
   document.head.appendChild(module);
 
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.crossOrigin = "anonymous";
-  link.href = "https://bukasz.github.io/utils/widget/assets/index-poqmf8AB.css?v=" + random;
+  link.href = "https://bukasz.github.io/utils/widget/assets/index--aKp-DgW.css?v=" + random;
   document.head.appendChild(link);
 })();
